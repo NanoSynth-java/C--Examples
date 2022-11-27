e9823ec49f19026180ee9822b6a47a4b2482432f
Daniil <Comm2k3@hotmail.com>
2022-11-27 17:25:47 +0200

    If Else example

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Даниил") {
+    Console.WriteLine("Привет любимый!");
+}
+
+else {
+    Console.Write("Привет ");
+    Console.Write(username);
+}
